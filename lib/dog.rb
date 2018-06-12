8e326a5fe46737a6b8cad2fb6f30cfdd1087c13e
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 05:39:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all.each { |dog| puts dog.name }
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+end 
